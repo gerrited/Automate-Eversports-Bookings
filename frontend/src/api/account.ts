37fb4bdf05a1c1e89466c56296e8cfa3c654bf95
Gerrit Edzards <gerrit.edzards@gmail.com>
2026-04-19 13:14:22 +0200

    fix: correct API endpoint in deleteAccount function to include '/api'

--- frontend/src/api/account.ts
+++ frontend/src/api/account.ts
@@ -1,5 +1,5 @@
 import { apiFetch } from './client'
 
 export function deleteAccount(): Promise<void> {
-  return apiFetch<void>('/account', { method: 'DELETE' })
+  return apiFetch<void>('/api/account', { method: 'DELETE' })
 }
